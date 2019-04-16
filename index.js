dcfe0d6bf6e0ffe5462a33dffd463606f31502b6
Nishant Kumar Jaiswal <nishant.soft04@gmail.com>
2019-04-16 10:23:24 +0530

    making call to SF API

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function clear_data() {
 clear_data();
 
 
-let MAX_DATA = 300;
+let MAX_DATA = 5000;
 
 app.get("/api/v1/add-sensor-data",function(req,res){
 	validateRequest(req);
@@ -39,7 +39,13 @@ app.get("/api/v1/add-sensor-data",function(req,res){
 	console.log("Received data for sensor " + sensor_id);
 	create_sensor_entry_if_not_present(sensor_id);
 	populate_sensor_data(req.query);
-	res.end("done");
+	create_salesforce_iot_entry(req.query, function(isDone){
+		if(isDone)
+			res.end("done");
+		else
+			res.json({error:"Not able to connect to Salesforce"});
+	})
+	
 });
 
 app.get("/api/v1/sensor-data/:sensor_id",function(req,res){
@@ -83,5 +89,57 @@ function populate_sensor_data(query){
 	if(sensor_data[sensor_id].data.length > MAX_DATA){
 		sensor_data[sensor_id].data.shift();
 	}
+	query.sensor_id = sensor_id;
 	console.log(JSON.stringify(sensor_data[sensor_id],null,4));
 }
+
+//------------SALESFORCE OPERATIONS------------
+var Client = require('node-rest-client').Client;
+var client = new Client();
+
+var create_iot_data_url = "https://curious-badger-r87z88-dev-ed.my.salesforce.com/services/data/v45.0/sobjects/Vehicle_Sensor__e/";
+function create_salesforce_iot_entry(query, callback){
+	let sensor_id = query.sensor_id;
+	console.log("populating data for sensor " + sensor_id);
+	delete query.sensor_id;
+	delete query.token;
+	delete query.received_at;
+	query.Registration_Number__c = sensor_id;
+	console.log("SF Query", query);
+	get_salesforce_access_token(function(token){
+		if(token == null){
+			callback(false);
+		}else{
+			var args = {
+				data: query,
+				headers: { "Content-Type": "application/json", "Accept": "application/json", "Authorization": "Bearer " + token }
+			}
+			client.post(create_iot_data_url,args, function(data, res){
+				console.log(data);
+				if(res.statusCode > 199 && res.statusCode < 210)
+					callback(true);
+				else
+					callback(false);
+			});
+		}
+	});
+}
+
+
+var args = {
+    data: {  },
+    headers: { "Content-Type": "application/json", "Accept": "application/json" }
+};
+var access_token_url = "https://curious-badger-r87z88-dev-ed.my.salesforce.com/services/oauth2/token?grant_type=refresh_token&client_id=3MVG9pe2TCoA1Pf70ZNevaF8fZ7myqRXECicQehJB5VolPJfKE45Q7MKcRRqtL072w3Me5PQ_clUw3dhm6H3s&client_secret=65404477F531C3201D0F83707F10FBD18B36E95B16D3EFE4AFBB08B040B9CFFE&refresh_token=5Aep861ARUdJp8j3X2crXPunKJUJD.oYplE6Y9nWeS12IiiUSzkpjmXx5amfF7vfJckcz.urX5Mqf56MfyUQJi5";
+function get_salesforce_access_token(callback){
+	client.post(access_token_url, args, function(data, res){
+		if(data.access_token != undefined && data.access_token != null){
+			callback(data.access_token);
+		}else{
+			callback(null);
+		}
+	});
+}
+
+
+
